6b2bf60c0d42ce8cecb92e3cdc63a500c6897908
sergioacos <arqsergioacosta@gmail.com>
2023-11-30 11:48:32 -0300

    se modifico conexion

--- Ingreso_Socios/Datos/Conexion.cs
+++ Ingreso_Socios/Datos/Conexion.cs
@@ -16,13 +16,69 @@ namespace Ingreso_Socios.Datos
         private string clave;
         private static Conexion? con = null;
 
-        public Conexion() 
+        public Conexion() // asignamos valores a las variables de la conexion
         {
+// variables usadas para larepetición de líneas de código
+            bool correcto = false;
+            int mensaje;    /* creamos las variables para recibir los datos desde el teclado
+//
+============================================================*/
+            string T_servidor = "Servidor" ;
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave"; // se antepuso la T para indicar que vienen desde TECLADO
+/*
+____________________________________________________________
+_
+* ciclo while para volver a repetir el ingreso de datos
+* la variable correcto la inicializamos para ingresar al
+ciclo
+*
+___________________________________________________________*/
+        while (correcto != true)
+        {
+// Armamos los cuadros de dialogo para el ingreso de datos
+            T_servidor = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
+            T_puerto = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
+            T_usuario = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
+            T_clave = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
+                /*
+________________________________________________________
+________________
+* controlamos que los datos ingresados para acceder a
+MySQL sean correctos
+*
+________________________________________________________
+__________________ */
+             mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " +
+                T_servidor + " PUERTO= " + T_puerto + " USUARIO: " +
+                                T_usuario + " CLAVE: " + T_clave,
+                "AVISO DEL SISTEMA", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+            else
+                {
+                    correcto = true;
+                }
+            }
+
+            // reemplazamos los datos concretos que teniamos por las variables
+
+
             this.baseDatos = "proyecto";
-            this.servidor = "localhost";
-            this.puerto = "3306";
-            this.usuario = "proyecto";
-            this.clave = "1234";
+            this.servidor = T_servidor;
+            this.puerto = T_puerto;
+            this.usuario = T_usuario;
+            this.clave = T_clave;
         }
         // proceso de interacción
         public MySqlConnection CrearConexion()
